476e5a1910edea35666c75331931b119e6f2e552
Surafeljava <surafelk27@gmail.com>
2022-03-12 15:19:55 +0300

    feat: Creating a telegram bot first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['Hello'])
+def greet(message):
+    bot.send_message(message.chat.id, "Hello There, This is a bot designed for the purpose of . . . ")
+    
+@bot.message_handler(commands=['?'])
+def help(message):
+    bot.reply_to(message, "Help is here . . .")
+
+print("Bot running . . .")
+
+
+bot.polling()
