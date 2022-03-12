0dfa0abdca63efec5ce23047a4364c6c8d4f14e0
Surafeljava <surafelk27@gmail.com>
2022-03-12 15:35:53 +0300

    feat: main update

--- main.py
+++ main.py
@@ -7,13 +7,22 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY)
 
+
 @bot.message_handler(commands=['Hello'])
 def greet(message):
-    bot.send_message(message.chat.id, "Hello There, This is a bot designed for the purpose of . . . ")
-    
+    bot.send_message(
+        message.chat.id, "Hello There, This is a bot designed for the purpose of . . . ")
+
+
 @bot.message_handler(commands=['?'])
 def help(message):
     bot.reply_to(message, "Help is here . . .")
+
+
+@bot.message_handler(commands=['hi'])
+def hi(message):
+    bot.reply_to(message, "Hii How are you doing? ")
+
 
 print("Bot running . . .")
 
